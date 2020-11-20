71b1fa51bb7f85c0c4a47c24ca6f5720c5520347
kostia bazrov <osetia-alania@mail.ru>
2020-11-20 21:13:23 +0300

    added dadata

---
+++ src/components/calc/Dadata.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <VueDadata />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Dadata',
+  components: {
+    VueDadata: () => import(/* ebpackChunkName: "vue-dadata" */ 'vue-dadata'),
+  },
+}
+</script>
