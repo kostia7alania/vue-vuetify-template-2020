e2a3b3bf382be2dfd950e79df2d1255574da9d75
Kostia Bazrov <k.bazrov@ngrsoftlab.ru>
2020-11-24 10:10:10 +0300

    dadata init version

--- src/components/calc/Dadata.vue
+++ src/components/calc/Dadata.vue
@@ -1,7 +1,13 @@
 <template>
   <section>
-    {{ constants }}
-    <VueDadata :token="constants.dadata" />
+    <VueDadata :token="constants.dadata" :on-change="onChange" />
+    <div v-if="Object.keys(form.data).length">
+      <div>Страна: {{ form.data.country }}</div>
+      <div>Город: {{ form.data.city }}</div>
+      <div>Улица: {{ form.data.street }}</div>
+      <div>Дом: {{ form.data.house }}</div>
+      <v-alert v-if="validMessage" border="top" color="red lighten-2" dark>{{ validMessage }}</v-alert>
+    </div>
   </section>
 </template>
 
@@ -16,7 +22,52 @@ export default {
   data() {
     return {
       constants,
+      form: {
+        data: {
+          /* postal_code: '107258',
+          country: 'Россия',
+          country_iso_code: 'RU',
+          federal_district: 'Центральный',
+          region_fias_id: '0c5b2444-70a0-4932-980c-b4dc0d3f02b5',
+          */
+        },
+        /*
+        unrestricted_value: '107258, г Москва, Богородское р-н, ул Глебовская, д 1',
+        value: 'г Москва, ул Глебовская, д 1',
+        */
+      },
     }
+  },
+  computed: {
+    validMessage() {
+      const { data } = this.form || {}
+      if (!Object.keys(data).length) return false
+
+      const errorsMap = {
+        country: 'Введите страну',
+        city: 'Введите город',
+        street: 'Введите улицу',
+        house: 'Введите дом',
+      }
+
+      const [, msg] = Object.entries(errorsMap).find(([key]) => !data[key]) || []
+      return msg
+    },
+  },
+  methods: {
+    onChange(form) {
+      console.log({ ...form.data })
+      this.form = form
+      if (this.validMessage) {
+        this.$emit('input', form)
+      }
+    },
   },
 }
 </script>
+
+<style lang="scss" scoped>
+::v-deep .vue-dadata__input {
+  min-width: 300px;
+}
+</style>
--- src/pages/Calc.vue
+++ src/pages/Calc.vue
@@ -2,15 +2,7 @@
   <v-container class="fill-height">
     <v-row justify="center" align="center">
       <v-col class="shrink">
-        <v-tooltip right>
-          <template v-slot:activator="{ on }">
-            <v-btn icon large target="_blank" v-on="on">
-              <v-icon large>mdi-code-tags</v-icon>
-            </v-btn>
-            <Dadata />
-          </template>
-          <span>Source</span>
-        </v-tooltip>
+        <Dadata />
       </v-col>
     </v-row>
   </v-container>
